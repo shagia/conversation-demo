a580a4450a16dca42d9196e2782dc658ff4e0822
shagia <mrkraiola@gmail.com>
2018-02-01 17:02:24 -0800

    major update
    more js, new words file, new index

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-//js file
-
-// $("#entity1").on("click", processClick());
-
 let state = [];
+//'state' dynamically stores what 'entities' are being selected
 
 function processClick(eleId){
 	if(state.length < 2){
@@ -18,45 +15,45 @@ function processClick(eleId){
 		state.push(eleId);
 		$('#' + eleId).addClass('activate');
 	}
+	//if there are less than 2 entities in state, eleID sends the entity to the state to fill the two spots, and .activate gives the entity in question a css rule. When more than 2 entities though, state resets, removes all .activate classes, and adds a new one on the latest clicked.
+
 	console.log(eleId);
 	console.log(state);
+	//used to debug. feel free to remove 
 
+	if (state.length >= 2) {
+		if(state.indexOf('entity1') !== -1 && state.indexOf('entity2') !==-1){
+				$("#chatLeft").text(convoOne.Left);
+				$("#chatRight").text(convoOne.Right);
+		}
+
+	//Upon selecting entity1 or entity2, the first conversation spawns
+		
+		else if(state.indexOf('entity1') !==-1 && state.indexOf('entity3') !==-1){
+				$("#chatLeft").text(convoTwo.Left);
+				$("#chatRight").text(convoTwo.Right);
+		}
+
+	//Upon selecting entity1 or entity3, the second conversation spawns
+
+		else if(state.indexOf('entity2') !==-1 && state.indexOf('entity3') !==-1){
+				$("#chatLeft").text(convoThree.Left);
+				$("#chatRight").text(convoThree.Right);
+		}
+
+	//Upon selecting entity2 or entity3, the third conversation spawns
+
+
+	} else {
+		$("#chatLeft").text("");
+		$("#chatRight").text("");
+	};
 }
 
+$(document).ready(function() {
 
-if (state == ["entity1", "entity3"]) {
-	console.log("fart");
+	});
 
-	};
-
-
-
-
-// $("#entity2").on("click", function() {
-// 	console.log("Entity 2 on.")
-// });
-
-// $("#entity3").on("click", function() {
-// 	console.log("Entity 3 on.")
-// });
-
-// $("#entity1").off("click", function() {
-// 	console.log("Entity 1 off.")
-// });
-
-// $("#entity2").off("click", function() {
-// 	console.log("Entity 2 off.")
-// });
-
-// $("#entity3").off("click", function() {
-// 	console.log("Entity 3 off.")
-// });
-
-// $("#entity1").trigger("click");
-
-// $("#buttons").click(function() {
-// 	$("#buttons").find("button").css("background-color","green");
-// });
 
 
 
---
+++ js/words.js
@@ -0,0 +1,15 @@
+var convoOne = {
+	Left:"This is the left side of the first conversation. You can find this existing in words.js. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+	Right:"This is the right side of the first conversation. You can find this also existing in words.js. Aenean commodo ligula eget dolor.",
+}
+
+var convoTwo = {
+	Left:"This is the left side of the second conversation. You can find this existing in words.js. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+	Right:"This is the right side of the second conversation. You can find this also existing in words.js. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+}
+
+var convoThree = {
+	Left:"This is the left side of the third conversation. You can find this existing in words.js. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.",
+	Right:"This is the right side of the third conversation. You can find this also existing in words.js. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.",
+}
+
